331206f3359532fe8de585aed930d2d58e8130c5
HemillyJulia <hemillyjulia@hotmail.com>
2022-09-12 16:20:41 -0400

    Fundamentos do react
    Nets aula criamos um arquivo utilizando o Vite, criamos o nosso primeiro componente e insermos propriedades a ele .

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {Post} from "./Post"
+
+export function App() {
+ 
+  return (
+    <div>
+<Post 
+author="Machado de Assis"
+content= "É considerado por muitos críticos o maior escritor brasileiro de todos os tempos, e sem dúvida um dos maiores de língua portuguesa. Foi autodidata - ou seja, conheceu os grandes nomes da literatura universal sozinho. Publicou seu primeiro poema aos 18 anos, período em que começou a trabalhar como jornalista, escrevendo artigos sobre política." 
+/>
+
+<Post 
+author="Carlos Drummond de Andrade"
+content= "Em termos de poesia, o itabirano Carlos Drummond de Andrade é talvez a maior referência. Formado em farmácia, já nos anos 20 iniciou sua carreira literária, com a fundação de uma revista modernista. Ao mesmo tempo em que trabalhava no Ministério da Educação do governo Getúlio Vargas entre 1934 e 1945, escrevia seus poemas."
+/>
+
+    </div>
+
+  )
+}
+
+
+
+
+
+
